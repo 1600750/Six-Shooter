3d3c0a767df3c0516b6dd9da3eb04ab92ebc9bf8
1600750 <1600750@uad.ac.uk>
2019-07-17 16:55:57 +0100

    Added basic fire mechanic

---
+++ Revolver/Assets/Player_Fire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Fire : MonoBehaviour {
+
+    public GameObject bullet;
+    public GameObject barrel;
+    public float speed = 100f;
+    bool fire;
+	// Use this for initialization
+	void Start ()
+    {
+        fire = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if((Input.GetAxis("Fire1") > 0 ) && fire == true)
+        {
+            GameObject instBullet = Instantiate(bullet, barrel.transform.position, Quaternion.identity) as GameObject;
+            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidbody.AddForce(barrel.transform.forward * speed);
+            fire = false;
+           
+        }
+
+        if ((Input.GetAxis("Fire1") == 0) && fire == false)
+        {
+            fire = true;
+        }
+    }
+}
