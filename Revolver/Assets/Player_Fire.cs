38a9bc559f7ce44e0006e9869c7183f70d42aa15
1600750 <1600750@uad.ac.uk>
2019-07-17 17:40:08 +0100

    Added Reload system and Can prefab

--- Revolver/Assets/Player_Fire.cs
+++ Revolver/Assets/Player_Fire.cs
@@ -7,23 +7,28 @@ public class Player_Fire : MonoBehaviour {
     public GameObject bullet;
     public GameObject barrel;
     public float speed = 100f;
+    int ammo;
     bool fire;
+    bool reload;
 	// Use this for initialization
 	void Start ()
     {
         fire = true;
+        ammo = 6;
+        reload = true;
 
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if((Input.GetAxis("Fire1") > 0 ) && fire == true)
+		if((Input.GetAxis("Fire1") > 0 ) && fire == true && ammo > 0)
         {
             GameObject instBullet = Instantiate(bullet, barrel.transform.position, Quaternion.identity) as GameObject;
             Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
             instBulletRigidbody.AddForce(barrel.transform.forward * speed);
             fire = false;
+            ammo--;
            
         }
 
@@ -31,5 +36,18 @@ public class Player_Fire : MonoBehaviour {
         {
             fire = true;
         }
+
+        if (Input.GetAxis("Reload") > 0 && reload == true && ammo < 6)
+        {
+            ammo++;
+            reload = false;
+        }
+
+        if ((Input.GetAxis("Reload") == 0) && reload == false)
+        {
+            reload = true;
+        }
+
+
     }
 }
