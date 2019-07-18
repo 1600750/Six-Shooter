50109239e39feb3396f5a81e7842d89f8f997b52
1600750 <1600750@uad.ac.uk>
2019-07-18 12:07:05 +0100

    Created array of gameobjects to store bullets in the chamber.

--- Revolver/Assets/Player_Fire.cs
+++ Revolver/Assets/Player_Fire.cs
@@ -5,30 +5,59 @@ using UnityEngine;
 public class Player_Fire : MonoBehaviour {
 
     public GameObject bullet;
+    GameObject[] cylander;
     public GameObject barrel;
     public float speed = 100f;
     public int ammo;
     bool fire;
     bool reload;
+    int j;
+    int r;
 	// Use this for initialization
 	void Start ()
     {
         fire = true;
         ammo = 6;
         reload = true;
+        cylander = new GameObject[6];
+        j = 0;
+        r = 0;
 
+        for(int i = 0; i < 6; i++)
+        {
+            cylander[i] = bullet;
+            
+        }
+
+       
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		if((Input.GetAxis("Fire1") > 0 ) && fire == true && ammo > 0)
+        if(j == 6)
         {
-            GameObject instBullet = Instantiate(bullet, barrel.transform.position, Quaternion.identity) as GameObject;
-            Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
-            instBulletRigidbody.AddForce(barrel.transform.forward * speed);
+            j = 0;
+        }
+
+        if (r == 6)
+        {
+            r = 0;
+        }
+
+        if ((Input.GetAxis("Fire1") > 0 ) && fire == true && ammo > 0)
+        {
+            if(cylander[j] != null)
+            {
+                GameObject instBullet = Instantiate(cylander[j], barrel.transform.position, Quaternion.identity) as GameObject;
+                Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+                instBulletRigidbody.AddForce(barrel.transform.forward * speed);
+                ammo--;
+                cylander[j] = null;
+            }
+
             fire = false;
-            ammo--;
+            j++;
            
         }
 
@@ -39,7 +68,15 @@ public class Player_Fire : MonoBehaviour {
 
         if (Input.GetAxis("Reload") > 0 && reload == true && ammo < 6)
         {
-            ammo++;
+            
+            if (cylander[r] == null)
+            {
+                cylander[r] = bullet;
+                ammo++;
+            }
+            
+            r++;
+            
             reload = false;
         }
 
